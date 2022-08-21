a7bd17277d66691e32374078569e59323acc8d4f
darshpatel123 <81002213+darshpatel123@users.noreply.github.com>
2022-08-21 14:04:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,21 +12,7 @@ function Female(){
    selected = true;
 }
 
-function submit(){
+
   
-Email.send({
-    Host : "smtp.gmail.com",
-    Username : "vklbverify@gmail.com",
-    Password : "kush_1503",
-    To : 'darshpatelkota@gmail.com',
-    From : "vklbverify@gmail.com",
-    Subject : "Regestration form VKLB"+document.getElementById("email").value+"      name:"+document.getElementById("name").value,
-    Body : "Email"+document.getElementById("email").value+
-      "      name:"+document.getElementById("name").value+
-      "       gender:"+document.getElementById("gender").value
-}).then(
-  message => alert("Form sended")
-)
-}
 
 
